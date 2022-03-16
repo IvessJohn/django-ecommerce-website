069bb8d3a6b50b947807d4607500494b666f3f11
IvessJohn <vanek2v@gmail.com>
2022-03-16 12:35:28 +0200

    Coupon assignment for orders implemented

--- ecommercewebsite/store/models.py
+++ ecommercewebsite/store/models.py
@@ -9,6 +9,7 @@ from coupon_management.models import Coupon, Discount
 
 # Create your models here.
 
+
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     name: models.CharField = models.CharField(max_length=200, null=True, blank=False)
@@ -20,28 +21,40 @@ class Customer(models.Model):
 
 class Product(models.Model):
     name: models.CharField = models.CharField(max_length=200, blank=False)
-    price: models.DecimalField = models.DecimalField(max_digits=11, decimal_places=2, blank=False, null=True)
-    digital: models.BooleanField = models.BooleanField(default=False, null=True, blank=True)
+    price: models.DecimalField = models.DecimalField(
+        max_digits=11, decimal_places=2, blank=False, null=True
+    )
+    digital: models.BooleanField = models.BooleanField(
+        default=False, null=True, blank=True
+    )
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self) -> str:
         return self.name
-    
+
     @property
     def imageURL(self):
         try:
             url = self.image.url
         except:
-            url = ''
+            url = ""
         return url
 
 
 class Order(models.Model):
-    customer: models.ForeignKey = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
-    date_ordered: models.DateTimeField = models.DateTimeField(auto_now_add=True, null=True)
+    customer: models.ForeignKey = models.ForeignKey(
+        Customer, on_delete=models.SET_NULL, null=True, blank=True
+    )
+    date_ordered: models.DateTimeField = models.DateTimeField(
+        auto_now_add=True, null=True
+    )
     complete: models.BooleanField = models.BooleanField(default=False)
-    transaction_id: models.CharField = models.CharField(max_length=100, null=True, unique=True)
-    applied_coupon = models.OneToOneField(Coupon, on_delete=models.DO_NOTHING, null=True, blank=True)
+    transaction_id: models.CharField = models.CharField(
+        max_length=100, null=True, unique=True
+    )
+    applied_coupon = models.OneToOneField(
+        Coupon, on_delete=models.SET_NULL, null=True, blank=True
+    )
 
     def __str__(self) -> str:
         return f"Order({self.id})-{self.customer.name}"
@@ -50,7 +63,7 @@ class Order(models.Model):
     def get_cart_price(self):
         orderitems = self.orderitem_set.all()
         return sum(item.get_total_price for item in orderitems)
-    
+
     @property
     def get_final_price(self):
         return self.__calculate_final_price()
@@ -59,44 +72,63 @@ class Order(models.Model):
     def get_items_amount(self):
         orderitems = self.orderitem_set.all()
         return sum(item.quantity for item in orderitems)
-    
+
     @property
     def requires_shipping(self):
         if len(self.orderitem_set.all()) == 0:
             return False
-        return any(orderitem.product.digital == False for orderitem in self.orderitem_set.all())
+        return any(
+            orderitem.product.digital == False for orderitem in self.orderitem_set.all()
+        )
+
+    @property
+    def get_discount_amount(self):
+        if self.coupon:
+            discount: Discount = self.applied_coupon.discount
+
+            if not discount.is_percentage:
+                return Decimal(discount.value)
+            else:
+                discount_percentage: int = min(discount.value, 100)
+                return self.get_cart_price * discount_percentage * 0.01
+
+        return Decimal(0.0)
 
     def apply_coupon(self, coupon_code):
-        user: User = self.customer__set.all()[0].user
+        print(str(self) + ": applying coupon - " + repr(coupon_code))
+        user: User = self.customer.user
 
         status = validate_coupon(coupon_code=coupon_code, user=user)
+        print(status["valid"])
         if status["valid"]:
             coupon: Coupon = Coupon.objects.get(code=coupon_code)
+            self.applied_coupon = coupon
             coupon.use_coupon(user=user)
+            self.save()
 
-    
     def __calculate_final_price(self):
         full_price: Decimal = self.get_cart_price
 
         if self.applied_coupon:
-            discount: Discount = self.applied_coupon.discount
-            
-            if discount.is_percentage:
-                discount_percentage: int = min(discount.value, 100)
-                discount_amount: Decimal = full_price * discount_percentage * 0.01
-            else:
-                discount_amount: Decimal = Decimal(discount.value)
+            discount_amount: Decimal = self.get_discount_amount
             return max(full_price - discount_amount, 0)
-        
+
         return full_price
 
-        
 
 class OrderItem(models.Model):
-    product: models.ForeignKey = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, unique=False)
-    order: models.ForeignKey = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True, unique=False)
-    quantity: models.IntegerField = models.IntegerField(default=0, null=True, blank=True)
-    date_added: models.DateTimeField = models.DateTimeField(auto_now_add=True, null=True)
+    product: models.ForeignKey = models.ForeignKey(
+        Product, on_delete=models.SET_NULL, null=True, unique=False
+    )
+    order: models.ForeignKey = models.ForeignKey(
+        Order, on_delete=models.SET_NULL, null=True, unique=False
+    )
+    quantity: models.IntegerField = models.IntegerField(
+        default=0, null=True, blank=True
+    )
+    date_added: models.DateTimeField = models.DateTimeField(
+        auto_now_add=True, null=True
+    )
 
     @property
     def get_total_price(self):
@@ -106,14 +138,26 @@ class OrderItem(models.Model):
 
 
 class ShippingInformation(models.Model):
-    customer: models.ForeignKey = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, unique=False)
-    order: models.ForeignKey = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True, unique=False)
-    address: models.CharField = models.CharField(max_length=200, null=False, blank=False)
+    customer: models.ForeignKey = models.ForeignKey(
+        Customer, on_delete=models.SET_NULL, null=True, unique=False
+    )
+    order: models.ForeignKey = models.ForeignKey(
+        Order, on_delete=models.SET_NULL, null=True, unique=False
+    )
+    address: models.CharField = models.CharField(
+        max_length=200, null=False, blank=False
+    )
     city: models.CharField = models.CharField(max_length=200, null=False, blank=False)
     state: models.CharField = models.CharField(max_length=200, null=False, blank=False)
-    country: CountryField = CountryField(blank_label="Select country...", default="USA", blank=True)
-    zipcode: models.CharField = models.CharField(max_length=200, null=False, blank=False)
-    date_added: models.DateTimeField = models.DateTimeField(auto_now_add=True, null=True)
+    country: CountryField = CountryField(
+        blank_label="Select country...", default="USA", blank=True
+    )
+    zipcode: models.CharField = models.CharField(
+        max_length=200, null=False, blank=False
+    )
+    date_added: models.DateTimeField = models.DateTimeField(
+        auto_now_add=True, null=True
+    )
 
     def __str__(self) -> str:
         return self.address
@@ -121,4 +165,3 @@ class ShippingInformation(models.Model):
     class Meta:
         verbose_name = "Shipping Information"
         verbose_name_plural = "Shipping Informations"
-
