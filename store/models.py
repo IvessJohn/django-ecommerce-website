1ed2b7b6a973249774df1c2fe1b728daf76acfc1
IvessJohn <vanek2v@gmail.com>
2022-04-03 11:41:49 +0300

    Update path for image URLs

--- store/models.py
+++ store/models.py
@@ -41,7 +41,7 @@ class Product(models.Model):
     def imageURL(self):
         """Return a valid image URL, or an empty string."""
         try:
-            url = self.image.url
+            url = "static" + self.image.url
         except:
             url = ""
         return url
